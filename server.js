f728b51a263210c03545ed1763265122d475a1d9
Aaroneld <merrifield48@gmail.com>
2020-01-29 18:08:02 -0500

    MVP

--- server.js
+++ server.js
@@ -6,4 +6,73 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', async (req, res, next) => {
+
+    try{
+        res.status(200).json(await db('accounts'));
+    } catch (err) {
+        next(err);
+    }
+
+
+});
+
+
+
+server.get('/:name', async (req, res, next) => {
+
+        const { name } = req.params 
+    try{
+        res.status(200).json(await db('accounts').where({name: name}));
+    } catch (err) {
+        next(err);
+    }
+});
+
+server.post('/', async (req, res, next) => {
+
+    const {name, budget} = req.body
+    try{
+        await db('accounts').insert({name: name, budget: budget});
+        res.status(200).json(await db('accounts').where({name: name}));
+    } catch (err) {
+        next(err);
+    }
+});
+
+server.put('/:name', async (req, res, next) => {
+    
+    const { name } = req.params;
+    const payload = {
+        name: req.body.name,
+        budget: req.body.budget
+    }
+   
+
+    try{
+ 
+        await db('accounts').where({name: name})
+            .update({name: payload.name, budget: payload.budget});
+        
+        res.status(201).json(await db('accounts').where({name: payload.name}).first());
+    } catch (err) {
+        next(err);
+    }
+});
+
+server.delete('/:name', async (req, res, next) => {
+    
+    const { name } = req.params;
+
+    try{
+
+        await db('accounts').where({name: name}).del();
+        res.status(204).end();
+
+    } catch (err) {
+        next(err);
+    }
+});
+
+
 module.exports = server;
